f4ebe6dee1432bd5a692f77a191148a3e84d908a
Throvtek <90162585+Throvtek@users.noreply.github.com>
2023-01-13 19:10:54 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     //const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://three-animestyle-v2.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
