76c0e9fceb14aa2438f128a3a7d4776a9946d401
Throvtek <90162585+Throvtek@users.noreply.github.com>
2023-01-13 23:07:15 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config();
 
+console.log(process.env.OPENAI_API_KEY);
+
+
 const configuration = new Configuration ({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -25,7 +28,32 @@ app.get('/', async (req, res) => {
     })
 });
 
-app.post('/imagenIA', async (req, res) => {
+// app.post('/', async (req, res) => {
+//     try {
+//         const prompt = req.body.prompt;
+
+//         const response = await openai.createCompletion ({
+//             model: "text-davinci-003",  //AI version
+//             prompt: `${prompt}`,        //We send the prompt that the user writes in our app
+//             temperature: 0,             //The risk AI takes when answering correct from 0 to 1 (0 low risk, 1 high risk)
+//              max_tokens: 3000,          //How many tokens can it take to answer (64 is small, 3000 is really long)
+//             top_p: 1,                   //
+//             frequency_penalty: 0.5,     //Likely to repeat the same answer (when asking similar question again)
+//             presence_penalty: 0,        //
+//             //stop: ["\"\"\""],         //We dont need an stop in our case
+//         })
+
+//         //We send it back to the frontend
+//         res.status(200).send({
+//             bot: response.data.choices[0].text
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send({ error })
+//     }
+// })
+
+app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
 
@@ -47,10 +75,7 @@ app.post('/imagenIA', async (req, res) => {
             size: "256x256",
           });
           //image_url = response.data.data[0].url;
-
-
-
-
+          
         //We send it back to the frontend
         res.status(200).send({
             bot:  response.data.data[0].url
